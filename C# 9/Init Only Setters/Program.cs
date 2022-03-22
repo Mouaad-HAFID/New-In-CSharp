33b93e9c13b14d032e861723aac0494984d369c8
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-22 16:04:30 +0100

    C# 9

--- C# 9/Init Only Setters/Program.cs
+++ C# 9/Init Only Setters/Program.cs
@@ -1,3 +1,32 @@
 ﻿//Init only setters provide consistent syntax to initialize members of an object. Property initializers make it clear which value is setting which property
 //The downside is that those properties must be settable.
-//
+//After initialization the properties become readonly and all eventual modifications are not permitted
+//this gives us a window for setting our property values and that window is the object creation
+//after an object is created we can no longer set values to our properties
+
+//Example:
+
+
+//Callers can use property initializer syntax to set the values, while still preserving the immutability:
+
+var now = new WeatherObservation
+{
+    RecordedAt = DateTime.Now,
+    TemperatureInCelsius = 20,
+    PressureInMillibars = 998.0m
+};
+
+//An attempt to change an observation after initialization results in a compiler error
+// Error! CS8852.
+//now.TemperatureInCelsius = 18;
+
+public struct WeatherObservation
+{
+    public DateTime RecordedAt { get; init; }
+    public decimal TemperatureInCelsius { get; init; }
+    public decimal PressureInMillibars { get; init; }
+
+    public override string ToString() =>
+        $"At {RecordedAt:h:mm tt} on {RecordedAt:M/d/yyyy}: " +
+        $"Temp = {TemperatureInCelsius}, with {PressureInMillibars} pressure";
+}
---
+++ C# 9/Top-level Statements/Program.cs
@@ -0,0 +1,59 @@
+﻿//Starting in C# 9, you don't have to explicitly include a Main method in a console application project
+//Instead, you can use the top-level statements feature to minimize the code you have to write. In this case, the compiler generates a class and Main method entry point for the application.
+//Top-level statements let you write simple programs for small utilities such as Azure Functions and GitHub Actions. They also make it simpler for new C# programmers to get started learning and writing code.
+//An application must have only one entry point. A project can have only one file with top-level statements.
+//Having more than one file containing top-level statements results in a compoiler error
+//We can write a Main method explicitly, but it can't function as an entry point. The compiler issues a warning
+//In a project with top-level statements, we can't use the -main compiler option to select the entry point, even if the project has one or more Main methods.
+//using directives must come first in the file
+//Top level statements are implicitly in the global namespace
+
+//Example Program
+using System.Text;
+
+StringBuilder builder = new();
+builder.AppendLine("Hello");
+builder.AppendLine("World!");
+
+Console.WriteLine(builder.ToString());
+
+//Top-level statements can reference the args variable to access any command-line arguments that were entered.
+//The args variable is never null but its Length is zero if no command-line arguments were provided
+
+if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine($"Argument={arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
+
+//Type definitions and namespaces must come after a top-level statement
+MyClass.TestMethod();
+MyNamespace.MyClass.MyMethod();
+
+public class MyClass
+{
+    public static void TestMethod()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+}
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public static void MyMethod()
+        {
+            Console.WriteLine("Hello World from MyNamespace.MyClass.MyMethod!");
+        }
+    }
+}
+
+
