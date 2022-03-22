82c1f47e82a8a460d9e1c2e656af4d121c25360e
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-22 17:18:17 +0100

    C# 8 | Using declarations

---
+++ C# 8/Using Declarations/Program.cs
@@ -0,0 +1,43 @@
+﻿//A using declaration is a variable declaration preceded by the using keyword. It tells the compiler that the variable being declared should be disposed at the end of the enclosing scope.
+
+//Example:
+static int WriteLinesToFile(IEnumerable<string> lines)
+{
+    using var file = new System.IO.StreamWriter("WriteLines2.txt");
+    int skippedLines = 0;
+    foreach (string line in lines)
+    {
+        if (!line.Contains("Second"))
+        {
+            file.WriteLine(line);
+        }
+        else
+        {
+            skippedLines++;
+        }
+    }
+    // Notice how skippedLines is in scope here.
+    return skippedLines;
+    // file is disposed here
+}
+
+//Classic using statement
+static int WriteLinesToFileClassic(IEnumerable<string> lines)
+{
+    using (var file = new System.IO.StreamWriter("WriteLines2.txt"))
+    {
+        int skippedLines = 0;
+        foreach (string line in lines)
+        {
+            if (!line.Contains("Second"))
+            {
+                file.WriteLine(line);
+            }
+            else
+            {
+                skippedLines++;
+            }
+        }
+        return skippedLines;
+    } // file is disposed here
+}
