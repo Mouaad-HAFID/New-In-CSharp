f3567ee5ef8520bfc92dfadc1314665092539ab9
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-22 14:51:45 +0100

    Revert "C# 7"
    This reverts commit 6b4406692c7b880ac54d55304ca8df1e145cfede.

--- C# 9/Init Only Setters/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿//Init only setters provide consistent syntax to initialize members of an object. Property initializers make it clear which value is setting which property
-//The downside is that those properties must be settable.
-//
