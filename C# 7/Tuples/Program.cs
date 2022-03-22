c6ce2b94340411fafa63fee10d1a1ab8e76e537a
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-22 14:53:38 +0100

    Initial commit

---
+++ C# 7/Tuples/Program.cs
@@ -0,0 +1,59 @@
+﻿//Available in C# 7.0 and later, the tuples feature provides concise syntax to group multiple data elements in a lightweight data structure. The following example shows how you can declare a tuple variable, initialize it, and access its data members
+(double, int) t1 = (4.5, 3);
+Console.WriteLine($"Tuple with elements {t1.Item1} and {t1.Item2}.");
+// Output:
+// Tuple with elements 4.5 and 3.
+
+(double Sum, int Count) t2 = (4.5, 3);
+Console.WriteLine($"Sum of {t2.Count} elements is {t2.Sum}.");
+// Output:
+// Sum of 3 elements is 4.5.
+
+//As the preceding example shows, to define a tuple type, you specify types of all its data members and, optionally, the field names
+//You cannot define methods in a tuple type, but you can use the methods provided by .NET
+(double, int) t = (4.5, 3);
+Console.WriteLine(t.ToString());
+Console.WriteLine($"Hash code of {t} is {t.GetHashCode()}.");
+// Output:
+// (4.5, 3)
+// Hash code of (4.5, 3) is 718460086.
+//Beginning with C# 7.3, tuple types support equality operators == and !=
+//You can define tuples with an arbitrary large number of elements
+
+
+//One of the most common use cases of tuples is as a method return type. That is, instead of defining out method parameters, you can group method results in a tuple return type
+
+var xs = new[] { 4, 7, 9 };
+var limits = FindMinMax(xs);
+Console.WriteLine($"Limits of [{string.Join(" ", xs)}] are {limits.min} and {limits.max}");
+// Output:
+// Limits of [4 7 9] are 4 and 9
+
+var ys = new[] { -9, 0, 67, 100 };
+var (minimum, maximum) = FindMinMax(ys);
+Console.WriteLine($"Limits of [{string.Join(" ", ys)}] are {minimum} and {maximum}");
+// Output:
+// Limits of [-9 0 67 100] are -9 and 100
+
+(int min, int max) FindMinMax(int[] input)
+{
+    if (input is null || input.Length == 0)
+    {
+        throw new ArgumentException("Cannot find minimum and maximum of a null or empty array.");
+    }
+
+    var min = int.MaxValue;
+    var max = int.MinValue;
+    foreach (var i in input)
+    {
+        if (i < min)
+        {
+            min = i;
+        }
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    return (min, max);
+}
---
+++ C# 9/Records/Program.cs
@@ -0,0 +1,56 @@
+﻿//C# 9.0 introduces record types. You use the record keyword to define a reference type that provides built-in functionality for encapsulating data
+//You can create record types with immutable properties by using positional parameters or standard property syntax
+namespace Program
+{
+    public record PersonPositionalParams(string FirstName, string LastName);
+    public record PersonStrandardPropertySyntax
+    {
+        public string FirstName { get; init; } = default!;
+        public string LastName { get; init; } = default!;
+    };
+    //We can create records with mutable properties and fields
+    public record PersonMutable
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+    };
+    //While records can be mutable, they are primarily intended for supporting immutable data models. 
+    /*
+    The record type offers the following features:
+
+        Concise syntax for creating a reference type with immutable properties
+        Behavior useful for a data-centric reference type:
+            Value equality
+            Concise syntax for nondestructive mutation
+            Built-in formatting for display
+        Support for inheritance hierarchies
+    */
+
+    //Value equality means that two variables of a record type are equal if the types match and all property and field values match.
+
+    public record Person(string FirstName, string LastName, string[] PhoneNumbers);
+
+    class Program
+    {
+        public static void Main()
+        {
+            var phoneNumbers = new string[2];
+            Person person1 = new("Nancy", "Davolio", phoneNumbers);
+            Person person2 = new("Nancy", "Davolio", phoneNumbers);
+            Console.WriteLine(person1 == person2); // output: True
+
+            person1.PhoneNumbers[0] = "555-1234";
+            Console.WriteLine(person1 == person2); // output: True
+
+            Console.WriteLine(ReferenceEquals(person1, person2)); // output: False
+        }
+    }
+
+    //Record types have a compiler-generated ToString method that displays the names and values of public properties and fields. The ToString method returns a string of the following format:
+    //<record type name> { <property name> = <value>, <property name> = <value>, ...}
+
+    //A record can inherit from another record. However, a record can't inherit from a class, and a class can't inherit from a record.
+
+    //For two record variables to be equal, the run-time type must be equal. The types of the containing variables might be different.
+
+}
