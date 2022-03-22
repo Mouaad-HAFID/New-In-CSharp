6b4406692c7b880ac54d55304ca8df1e145cfede
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-22 14:50:02 +0100

    C# 7

---
+++ C# 8/Asynchronous Streams/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Starting with C# 8.0, you can create and consume streams asynchronously. A method that returns an asynchronous stream has three properties:
+
+    1-It's declared with the async modifier.
+    2-It returns an IAsyncEnumerable<T>.
+    3-The method contains yield return statements to return successive elements in the asynchronous stream.
+*/
+
+using System.Collections;
+using System.Threading;
+
+static async System.Collections.Generic.IAsyncEnumerable<int> GenerateSequence()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        await Task.Delay(100);
+        yield return i;
+    }
+}
+
+await foreach (var number in GenerateSequence())
+{
+    Console.WriteLine(number);
+}
---
+++ C# 8/Readonly Members/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Program
+{
+    public struct Point
+    {
+        //The compiler doesn't assume get accessors don't modify state; you must declare readonly explicitly.
+        //Auto-implemented properties are an exception; the compiler will treat all auto-implemented getters as readonly.
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        //Marking Distance as readonly removes the warning
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+
+        //Like most structs, the ToString() method doesn't modify state. You could indicate that by adding the readonly modifier to the declaration of ToString()
+        //This generates a compiler warning because ToString accesses the Distance property, which isn't marked readonly
+        public readonly override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+    }
+}
